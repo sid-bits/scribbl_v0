150ff26db1d93dcc12c43ad22f767cbe8d9e7878
siddharth <f20190539@pilani.bits-pilani.ac.in>
2022-03-09 20:55:15 +0530

    initial project setup

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(4000, () => {
+  console.log("Application running on http://localhost:4000/");
+});
---
+++ frontend/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+  return (
+    <>
+      <div>Scribbl coming soon!</div>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
